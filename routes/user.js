22cf75ca0c1d30de4ef2c530d5d2387af6aeece1
CamilaReyes97 <camila.reyesber97@gmail.com>
2021-01-08 18:23:15 -0500

    Se agragaron comentarios

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,11 @@
+/* Rutas de los Usuarios */
 module.exports = (app) => {
     const user = require('../controllers/userController')
 
-    app.post('/user/create', user.create)
-    app.get('/user/get', user.findAll)
-    app.get('/user/getWeb', user.findAllWeb) //filtro
-    app.get('/user/getOne/:id', user.findOne)
-    app.put('/user/update/:id', user.update)
-    app.delete('/user/delete/:id', user.delete)
+    app.post('/user/create', user.create) /* Crear un usuario */
+    app.get('/user/get', user.findAll) /* Mostrar todos los usuarios */
+    app.get('/user/getOne/:id', user.findOne) /* Buscar un usuario en especifico */
+    app.put('/user/update/:id', user.update) /* Actualizar un usuario por id */
+    app.delete('/user/delete/:id', user.delete) /* Eliminar un usuario por id */
 
 }
